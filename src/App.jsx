67db2d9269dc7ce622ef6de35052d61ca4a79228
udita <uditagowdety@gmail.com>
2024-12-19 11:05:12 +0530

    starting about section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/home/Home";
+import About from "./components/about/About";
 
 function App() {
   
