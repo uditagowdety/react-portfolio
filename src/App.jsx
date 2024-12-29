6a9abd1c998cb75c3a421e647540878acdfd5e8b
udita <uditagowdety@gmail.com>
2024-12-29 12:22:11 +0530

    started qualifications section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
+import Qualification from "./components/qualification/Qualification";
 
 const App =() => {
   
@@ -15,6 +16,7 @@ const App =() => {
         <Home />
         <About />
         <Skills />
+        <Qualification />
       </main>
     </>
   )
