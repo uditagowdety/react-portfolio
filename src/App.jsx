9b3e9a1367e756db93dd96990204b870281409f9
udita <uditagowdety@gmail.com>
2024-12-30 10:04:26 +0530

    contact section barebones setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/home/Home";
 import About from "./components/about/About";
 import Skills from "./components/skills/Skills";
 import Qualification from "./components/qualification/Qualification";
+import Contact from "./components/contact/Contact";
 
 const App =() => {
   
@@ -17,6 +18,7 @@ const App =() => {
         <About />
         <Skills />
         <Qualification />
+        <Contact />
       </main>
     </>
   )
